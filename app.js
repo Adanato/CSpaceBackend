ecfd19ab1b9cf16e89715bb6525c0a4ab8b9e259
Adanato <66979547+Adanato@users.noreply.github.com>
2023-08-09 15:35:58 -0400

    adding auth routes for user login

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
+//Routes
 const Posts = require("./routes/Posts");
+const Auth = require("./routes/Auth");
 
 const connectDB = require("./db/connect");
 require("dotenv").config();
@@ -14,6 +17,7 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 app.use("/api/v1/posts", Posts);
+app.use("/api/v1/auth", Auth);
 app.use(notFound);
 app.use(errorHandler);
 const port = process.env.PORT || 3000;
