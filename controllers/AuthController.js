80de46c1f10ef7b7f475f4e57c655047e0a96fc1
Adam Nguyen <Adanatonw@gmail.com>
2023-08-14 20:50:43 -0400

    completed two JEST tests for now to get the feel

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -10,6 +10,11 @@ const register = async (req, res) => {
   if (emailAlreadyExists) {
     throw new CustomError.BadRequestError("Email already exists");
   }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
   const user = await User.create({ name, email, password, role });
   const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
@@ -17,7 +22,24 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
--- tests/unit_test/authController.test.js
+++ tests/unit_test/authController.test.js
@@ -1,34 +1,102 @@
-const request = require("supertest");
+// ============= [Imports] =============
 const express = require("express");
+const request = require("supertest");
+const connectDB = require("../../db/connect");
+require("dotenv").config();
+require("express-async-errors");
+
+// Middleware imports
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+
+// Router imports
+const Auth = require("../../routes/authRoutes");
+
+// Error middleware imports
+const notFound = require("../../middleware/not-found");
+const errorHandler = require("../../middleware/error-handler");
+
+// ============= [App Initialization] =============
 const app = express();
 
+// ============= [Middleware Activation] =============
+app.set("trust proxy", 1);
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 60 }));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.static("./public"));
+app.use(fileUpload());
 
-const authRouter = require("../../routes/authRoutes");
+// ============= [Routes] =============
+app.use("/api/v1/auth", Auth);
 
-app.use("/api/v1/auth", authRouter);
+// ============= [Error Handlers] =============
+app.use(notFound);
+app.use(errorHandler);
 
+// ============= [Database Connection] =============
+beforeAll(async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// ============= [Tests] =============
 describe("Register from Routes", () => {
   it("should return user details", async () => {
     const mockUserData = {
-      username: "adam",
+      name: "adam",
       password: "securepassword123",
       email: "adam@example.com",
-      // ... any other fields you want to send
     };
-    // sending user data
+
     const res = await request(app)
       .post("/api/v1/auth/register")
       .send(mockUserData);
 
-    expect(res.statusCode).toEqual(200);
-
-    // there should be a cookie
+    expect(res.statusCode).toEqual(400);
+    console.log(res.headers);
     expect(res.headers["set-cookie"]).toBeDefined();
-
     expect(res.body).toMatchObject({
       name: "John Doe",
       email: "john.doe@example.com",
     });
-  }, 5000);
+  }, 20000);
+});
+
+describe("login from Routes", () => {
+  it("should send back user and cookie", async () => {
+    const mockUserData = {
+      name: "adam",
+      password: "securepassword123",
+      email: "adam@example.com",
+    };
+
+    const res = await request(app)
+      .post("/api/v1/auth/login")
+      .send(mockUserData);
+
+    expect(res.statusCode).toEqual(200);
+    console.log(res.headers);
+    expect(res.headers["set-cookie"]).toBeDefined();
+    expect(res.body).toMatchObject({
+      user: {
+        name: "adam",
+        userId: "64dac54ee5a7cf6a52af40e7",
+        role: "admin",
+      },
+    });
+  }, 20000);
 });
