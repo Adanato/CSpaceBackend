9fb805805275699378c414b6ace803acbdbaa542
Adam Nguyen <Adanatonw@gmail.com>
2023-08-14 20:07:56 -0400

    replaced posts controllers

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -1,5 +1,20 @@
 const Post = require("../models/Post");
 
+const getAllPosts = async (req, res) => {
+  res.send("route");
+};
+const createPost = async (req, res) => {
+  res.send("route");
+};
+const getPost = async (req, res) => {
+  res.send("route");
+};
+const updatePost = async (req, res) => {
+  res.send("route");
+};
+const deletePost = async (req, res) => {
+  res.send("route");
+};
 module.exports = {
   getAllPosts,
   createPost,
