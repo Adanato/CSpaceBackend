4ffe2fbe15650ef1cd19471570110c9ff700ac19
Adam Nguyen <Adanatonw@gmail.com>
2023-08-23 19:21:37 -0400

    fixed misnamed postsController

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+//
 const { register, login, logout } = require("../controllers/authController.js");
 
 router.route("/login").post(login);
